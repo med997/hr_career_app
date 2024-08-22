2397716958378344a133e2217fd256005dbde6a1
ahmedafeef <ahmedafeef1999@gmail.com>
2024-08-22 07:28:21 +0300

    add company_appbar.dart, nav_to_sevices.dart google_background.png, google_icon.png,,, update AndroidManifest.xml, pubspec.yaml

---
+++ lib/features/company/presentation/widgets/company_appbar.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:hr_career_platform/core/app_theme.dart';
+import 'package:hr_career_platform/core/widgets/image_holder.dart';
+import '../../../../core/util/nav_to_sevices.dart';
+import '../../../../core/widgets/text_with_icon.dart';
+
+AppBar jobsAppBarFunction(
+  String companyName,
+  String companyMajor,
+  String companyLocation,
+  String companyLogo,
+  String backgroundCompanyImg,
+  String companyNumber,
+  String companyEmail,
+  String companyWebsite,
+) {
+  return AppBar(
+    title: Padding(
+      padding: const EdgeInsets.only(bottom: 7.0, right: 7.0, left: 7.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const BackButton(
+            color: Colors.white,
+          ),
+          ListTile(
+            leading: CircleAvatar(
+                child: ClipOval(
+              child: ImageHolder(url: companyLogo),
+            )),
+            title: Text(
+              companyName,
+              style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 16),
+            ),
+            subtitle: Wrap(
+              spacing: 10,
+              children: [
+                Text(
+                  companyMajor,
+                  style: const TextStyle(color: Colors.white, fontSize: 14),
+                ),
+                TextWithIcon(
+                  icon: const Icon(
+                    Icons.place_outlined,
+                    color: primaryColor,
+                  ),
+                  text: companyLocation,
+                  textColor: Colors.white,
+                )
+              ],
+            ),
+          ),
+          Wrap(
+            alignment: WrapAlignment.start,
+            runAlignment: WrapAlignment.start,
+            spacing: 15,
+            runSpacing: 10,
+            children: [
+              CircularIconButton(
+                icon: Icons.call_outlined,
+                onPressed: () {
+                  navToCall(companyNumber);
+                },
+              ),
+              CircularIconButton(
+                icon: Icons.mail_outlined,
+                onPressed: () {
+                  navToEmail(companyEmail);
+                },
+              ),
+              CircularIconButton(
+                icon: Icons.language_outlined,
+                onPressed: () {
+                  navToWebsite(companyWebsite);
+                },
+              ),
+              CircularIconButton(
+                icon: Icons.send_outlined,
+                onPressed: () {
+                  MapUtils.navToMap(-3.823216, -38.481700);
+                },
+              ),
+              CircularIconButton(
+                icon: Icons.more_horiz_rounded,
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ],
+      ),
+    ),
+    flexibleSpace: Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+            opacity: 0.5,
+            image: AssetImage(backgroundCompanyImg),
+            fit: BoxFit.fitWidth, // Adjust fit as needed
+          ),
+          borderRadius: const BorderRadius.all(Radius.circular(40))),
+    ),
+    toolbarHeight: 200,
+  );
+}
+
+class CircularIconButton extends StatelessWidget {
+  const CircularIconButton(
+      {super.key, required this.icon, required this.onPressed});
+
+  final IconData icon;
+  final Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton.filled(
+        iconSize: 20,
+        onPressed: onPressed,
+        icon: Icon(
+          icon,
+          color: Colors.white,
+        ),
+        style: IconButton.styleFrom(
+          backgroundColor: primaryColor.withOpacity(0.5),
+          hoverColor: primaryTransparent,
+        ));
+  }
+}
