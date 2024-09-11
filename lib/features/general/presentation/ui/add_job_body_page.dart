f824f4a4604cf265f0a72f791916b908b668c9a5
mohammed <meddce1997@gmail.com>
2024-09-12 02:40:11 +0300

    new editions in dynamic forms

--- lib/features/general/presentation/ui/add_job_body_page.dart
+++ lib/features/general/presentation/ui/add_job_body_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hr_career_platform/core/app_theme.dart';
+import 'package:hr_career_platform/core/cubit/dynamic_form_cubit.dart';
 import 'package:hr_career_platform/core/model/dynamic_model.dart';
 import 'package:hr_career_platform/core/util/enums.dart';
 import 'package:hr_career_platform/core/util/validator.dart';
@@ -24,170 +25,177 @@ class AddJobBodyPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      shrinkWrap: true,
-      children: [
-        BlocBuilder<GeneralCubit, GeneralState>(
-          builder: (context, state) {
-            if (state is GeneralLoading) {
-              return Center(
-                child: LoadingWidget(),
-              );
-            } else if (state is GeneralFetchedState) {
-              return Column(
-                children: [
-                  Center(
-                    child: DynamicFormWidget(
-                        formKey: addJobFormKey,
-                        dynamicFormsList: [
-                          DynamicModel('Job Title', FormType.text,
-                              validators: [
-                                DynamicFormValidator(
-                                    ValidatorType.notEmpty, 'isRequired')
-                              ],
-                              value: '',
-                              isRequired: true,
-                              disabled: false),
-                          DynamicModel('Deadline', FormType.datePicker,
-                              value: '',
-                              isRequired: true,
-                              validators: [
-                                DynamicFormValidator(
-                                    ValidatorType.notEmpty, 'isRequired')
-                              ],
-                              disabled: false),
-                          DynamicModel('Qualifications', FormType.dropdown,
-                              validators: [
-                                DynamicFormValidator(
-                                    ValidatorType.notEmpty, 'isRequired')
-                              ],
-                              value: '',
-                              items: state.generals.qualifications
-                                  .map(
-                                    (e) => ItemModel(key: e, value: e),
-                                  )
-                                  .toList(),
-                              isRequired: true,
-                              disabled: false),
-                          DynamicModel('Nationality', FormType.dropdown,
-                              validators: [
-                                DynamicFormValidator(
-                                    ValidatorType.notEmpty, 'isRequired')
-                              ],
-                              value: '',
-                              items: state.generals.nationality
-                                  .map(
-                                    (e) => ItemModel(key: e, value: e),
-                                  )
-                                  .toList(),
-                              isRequired: true,
-                              disabled: false),
-                          DynamicModel('Gender', FormType.dropdown,
-                              validators: [
-                                DynamicFormValidator(
-                                    ValidatorType.notEmpty, 'isRequired')
-                              ],
-                              value: '',
-                              items: state.generals.gender
-                                  .map(
-                                    (e) => ItemModel(key: e, value: e),
-                                  )
-                                  .toList(),
-                              isRequired: true,
-                              disabled: false),
-                          DynamicModel('Time parts', FormType.dropdown,
-                              validators: [
-                                DynamicFormValidator(
-                                    ValidatorType.notEmpty, 'isRequired')
-                              ],
-                              value: '',
-                              isRequired: true,
-                              disabled: false),
-                          DynamicModel('Other apply links', FormType.text,
-                              validators: [
-                                DynamicFormValidator(
-                                    ValidatorType.notEmpty, 'isRequired')
-                              ],
-                              value: '',
-                              isRequired: true,
-                              disabled: false),
-                          DynamicModel('Office Type', FormType.dropdown,
-                              validators: [
-                                DynamicFormValidator(
-                                    ValidatorType.notEmpty, 'isRequired')
-                              ],
-                              value: '',
-                              items: state.generals.officeType
-                                  .map(
-                                    (e) => ItemModel(key: e, value: e),
-                                  )
-                                  .toList(),
-                              isRequired: true,
-                              disabled: false),
-                          DynamicModel(
-                            'Address',
-                            FormType.text,
-                            validators: [
-                              DynamicFormValidator(
-                                  ValidatorType.notEmpty, 'isRequired')
-                            ],
-                            value: '',
-                            isRequired: true,
-                            disabled: false,
-                            action: IconButton(
-                              onPressed: () {
-                                Navigator.of(context).push(MaterialPageRoute(builder:(context) => LocationWidget(),));
-                              },
-                              icon: const Icon(
-                                Icons.location_on,
-                                color: primaryColor,
-                              ),
+    return BlocProvider(
+      create: (context) => DynamicFormCubit(),
+      child: ListView(
+        shrinkWrap: true,
+        children: [
+          BlocBuilder<GeneralCubit, GeneralState>(
+            builder: (context, state) {
+              if (state is GeneralLoading) {
+                return Center(
+                  child: LoadingWidget(),
+                );
+              } else if (state is GeneralFetchedState) {
 
-                            ),
-                          ),
-                          DynamicModel('Job Description', FormType.multiline,
-                              validators: [
-                                DynamicFormValidator(
-                                    ValidatorType.notEmpty, 'isRequired')
-                              ],
-                              value: '',
-                              isRequired: true,
-                              disabled: false),
-                          DynamicModel('Job requirement', FormType.multiline,
-                              validators: [
-                                DynamicFormValidator(
-                                    ValidatorType.notEmpty, 'isRequired')
-                              ],
-                              value: '',
-                              isRequired: true,
-                              disabled: false),
-                        ],
-                        useResponsiveUi: false),
-                  ),
-                  FloatingActionButton(
-                      child: const Icon(
-                        Icons.navigate_next,
-                        color: Colors.white,
-                      ),
+                List<DynamicModel> addjobForm = [
+                  DynamicModel('Job Title', FormType.text,
+                      validators: [
+                        DynamicFormValidator(
+                            ValidatorType.notEmpty, 'isRequired')
+                      ],
+                      value: '',
+                      isRequired: true,
+                      disabled: false),
+                  DynamicModel('Deadline', FormType.datePicker,
+                      value: '',
+                      isRequired: true,
+                      validators: [
+                        DynamicFormValidator(
+                            ValidatorType.notEmpty, 'isRequired')
+                      ],
+                      disabled: false),
+                  DynamicModel('Qualifications', FormType.dropdown,
+                      validators: [
+                        DynamicFormValidator(
+                            ValidatorType.notEmpty, 'isRequired')
+                      ],
+                      value: '',
+                      items: state.generals.qualifications
+                          .map(
+                            (e) => ItemModel(key: e, value: e),
+                      )
+                          .toList(),
+                      isRequired: true,
+                      disabled: false),
+                  DynamicModel('Nationality', FormType.dropdown,
+                      validators: [
+                        DynamicFormValidator(
+                            ValidatorType.notEmpty, 'isRequired')
+                      ],
+                      value: '',
+                      items: state.generals.nationality
+                          .map(
+                            (e) => ItemModel(key: e, value: e),
+                      )
+                          .toList(),
+                      isRequired: true,
+                      disabled: false),
+                  DynamicModel('Gender', FormType.dropdown,
+                      validators: [
+                        DynamicFormValidator(
+                            ValidatorType.notEmpty, 'isRequired')
+                      ],
+                      value: '',
+                      items: state.generals.gender
+                          .map(
+                            (e) => ItemModel(key: e, value: e),
+                      )
+                          .toList(),
+                      isRequired: true,
+                      disabled: false),
+                  DynamicModel('Time parts', FormType.dropdown,
+                      validators: [
+                        DynamicFormValidator(
+                            ValidatorType.notEmpty, 'isRequired')
+                      ],
+                      value: '',
+                      isRequired: true,
+                      disabled: false),
+                  DynamicModel('Other apply links', FormType.text,
+                      validators: [
+                        DynamicFormValidator(
+                            ValidatorType.notEmpty, 'isRequired')
+                      ],
+                      value: '',
+                      isRequired: true,
+                      disabled: false),
+                  DynamicModel('Office Type', FormType.dropdown,
+                      validators: [
+                        DynamicFormValidator(
+                            ValidatorType.notEmpty, 'isRequired')
+                      ],
+                      value: '',
+                      items: state.generals.officeType
+                          .map(
+                            (e) => ItemModel(key: e, value: e),
+                      )
+                          .toList(),
+                      isRequired: true,
+                      disabled: false),
+                  DynamicModel(
+                    'Address',
+                    FormType.text,
+                    validators: [
+                      DynamicFormValidator(
+                          ValidatorType.notEmpty, 'isRequired')
+                    ],
+                    value: '',
+                    isRequired: true,
+                    disabled: false,
+                    action: IconButton(
                       onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => stepperPageBody(1)));
-                      }),
-                ],
-              );
-            } else if (state is GeneralErrorState) {
-              print('GeneralErrorState');
-              return Text(
-                state.msg,
-                style: TextStyle(color: Colors.red),
-              );
-            }
-            return SizedBox();
-          },
-        ),
-      ],
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => LocationWidget(),));
+                      },
+                      icon: const Icon(
+                        Icons.location_on,
+                        color: primaryColor,
+                      ),
+
+                    ),
+                  ),
+                  DynamicModel('Job Description', FormType.multiline,
+                      validators: [
+                        DynamicFormValidator(
+                            ValidatorType.notEmpty, 'isRequired')
+                      ],
+                      value: '',
+                      isRequired: true,
+                      disabled: false),
+                  DynamicModel('Job requirement', FormType.multiline,
+                      validators: [
+                        DynamicFormValidator(
+                            ValidatorType.notEmpty, 'isRequired')
+                      ],
+                      value: '',
+                      isRequired: true,
+                      disabled: false),
+                ];
+                context.read<DynamicFormCubit>().addAllFields(addjobForm);
+                return Column(
+                  children: [
+                    Center(
+                      child: DynamicFormWidget(
+                          formKey: addJobFormKey,
+                          dynamicFormsList: addjobForm,
+                          useResponsiveUi: false),
+                    ),
+                    FloatingActionButton(
+                        child: const Icon(
+                          Icons.navigate_next,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => stepperPageBody(1)));
+                        }),
+                  ],
+                );
+              } else if (state is GeneralErrorState) {
+                print('GeneralErrorState');
+                return Text(
+                  state.msg,
+                  style: TextStyle(color: Colors.red),
+                );
+              }
+              return SizedBox();
+            },
+          ),
+        ],
+      ),
     );
   }
 
@@ -200,13 +208,12 @@ class AddJobBodyPage extends StatelessWidget {
       currentLatLng: const LatLng(22.968509, 44.917676),
       onNext: (GeocodingResult? result) {
         if (result != null) {
-
-            print('next ${result.formattedAddress}');
-            print('next ${result.geometry.location.toString()}');
-            result.formattedAddress ?? "";
-          }else
-            return SizedBox();
-        },
+          print('next ${result.formattedAddress}');
+          print('next ${result.geometry.location.toString()}');
+          result.formattedAddress ?? "";
+        } else
+          return SizedBox();
+      },
 
     );
   }
